86cce0c7dfdeb1a7f3d3fedea5e28496ba28f7d1
klekovvlad <100075182+klekovvlad@users.noreply.github.com>
2022-05-11 21:20:11 +0300

    Add files via upload

---
+++ nareke2/sent.php
@@ -0,0 +1,55 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+
+
+
+// Формирование самого письма
+$title = "Заказ работы";
+$body = "
+<h2>Клиент</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'nareketest@mail.ru'; // Логин на почте
+    $mail->Password   = '9Szw5re2eFrTUihNyPst
+'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('nareketest@mail.ru', 'НА РЕКЕ'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('klekov23@yandex.ru');  
+   
+
+   
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
